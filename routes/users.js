50dc1bb995d7eb95381b14207dd9e79f347e697d
Rhubert710 <73670513+Rhubert710@users.noreply.github.com>
2022-05-17 18:14:10 -0400

    myname, datetime and myfavoritemovies ;2C

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,21 @@
 var express = require('express');
 var router = express.Router();
 
+
 /* GET users listing. */
 router.get('/', function(req, res, next) {
   res.send('respond with a resource');
 });
+
+//myname
+router.get('/myname', function(req, res, next) {
+  res.send('my name is Robert');
+});
+
+//myfavoritemovies 
+router.get('/myfavoritemovies', function(req, res, next) {
+  res.json(['a, b']);
+});
+
 
 module.exports = router;
